db52acc6b5240b5f3330a53fd506abea4ebd3a7d
webnocode <gdfajardo6@soy.sena.edu.co>
2025-02-22 15:49:28 -0500

    solucion reproductor flotante

--- src/pages/music/[id].js
+++ src/pages/music/[id].js
@@ -141,32 +141,96 @@ export default function Music() {
                   </div>
                   <ImageAndText content={musicContent} onItemClick={handleItemClick} />
               </div>
-              <div className='backgroundColor2' style={{ padding: '10px', position: 'relative', zIndex: 2, borderRadius: '20px', margin: '10px', maxHeight: '80vh', overflowY: 'auto' }}>
-                  <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
-                      <MenuIcon onClick={toggleMenu} /> {/* Aquí se activa el menú */}
-                      <div style={{ maxWidth: '30vw', overflow: 'auto' }}>
-                          <ImageAndText content={content} onItemClick={handleItemClick} />
-                      </div>
-                      <div className="input-container backgroundColor2" onClick={(e) => e.stopPropagation()}>
-                          <SeacrhTagInDb tags={tags} setTags={setTags} setContent={setContent} setMusicContent={setMusicContent} />
-                      </div>
-                      <ToggleIconOpenAndClose size={30} isOpen={isContentVisible} onToggle={toggleContentVisibility} />
-                      <div className={isVideoFullScreen ? 'video-fullscreen' : 'video-normal'} style={{ position: isVideoFullScreen ? 'fixed' : 'relative', top: isVideoFullScreen ? '0' : 'auto', left: isVideoFullScreen ? '0' : 'auto', zIndex: isVideoFullScreen ? 9999 : 'auto' }}>
-                          <Video tags={tags} setTags={setTags} setCurrentIndex={setCurrentIndex} currentIndex={currentIndex} currentTimeMedia={currentTimeMedia} setCurrentTimeMedia={setCurrentTimeMedia} componentInUse={componentInUse} setComponentInUse={setComponentInUse} id={content[0].videoPrincipal.id} src={content[0].videoPrincipal.src} style={{ width: '100%', height: '100%' }} className={[]} onClick={() => console.log('Video clicked')} setIsLoading={setIsLoading} isVideoFullScreen={isVideoFullScreen} allMusicProyects={musicContent} setContent={setContent} isEndendVideo={isEndedVideo} setIsEndedVideo={setIsEndedVideo} setMusicContent={setMusicContent} />
-                          <div onClick={toggleVideoFullScreen} style={{ position: 'absolute', top: '0px', right: '0', zIndex: 10000, cursor: 'pointer', backgroundColor: 'none', padding: '0px', display: 'flex', alignItems: 'center', justifyContent: 'center', width: 'auto' }}>
-                              {isVideoFullScreen ? (
-                                  <ShrinkIcon onClick={toggleVideoFullScreen} size={50} />
-                              ) : (
-                                  <ExpandIcon onClick={toggleVideoFullScreen} size={50} />
-                              )}
-                          </div>
-                      </div>
-                  </div>
-                  <div style={{ opacity: isContentVisible ? 1 : 0, maxHeight: isContentVisible ? '70vh' : '0', overflow: 'hidden', transition: 'opacity 2s ease, max-height 2s ease', display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}>
-                      <MidiAndPdf content={content} onItemClick={handleItemClick} />
-                  </div>
-                  <Audio setCurrentIndex={setCurrentIndex} currentIndex={currentIndex} id={content[0].audioPrincipal.id} src={content[0].audioPrincipal.src} autoPlay={content[0].audioPrincipal.autoPlay} loop={content[0].audioPrincipal.loop} controlsList={content[0].audioPrincipal.controlsList} backgroundColor={content[0].audioPrincipal.backgroundColor} buttonColor={content[0].audioPrincipal.buttonColor} sliderEmptyColor={content[0].audioPrincipal.sliderEmptyColor} sliderFilledColor={content[0].audioPrincipal.sliderFilledColor} showPlayButton={content[0].audioPrincipal.showPlayButton} showVolumeButton={content[0].audioPrincipal.showVolumeButton} playIcon={content[0].audioPrincipal.playIcon} pauseIcon={content[0].audioPrincipal.pauseIcon} volumeIcon={content[0].audioPrincipal.volumeIcon} width={content[0].audioPrincipal.width} allMusicProyects={musicContent} setContent={setContent} setCurrentTimeMedia={setCurrentTimeMedia} currentTimeMedia={currentTimeMedia} setComponentInUse={setComponentInUse} componentInUse={componentInUse} setIsLoading={setIsLoading} isEndedVideo={isEndedVideo} setIsEndedVideo={setIsEndedVideo} />
-              </div>
+              <div 
+  className='backgroundColor2' 
+  style={{ 
+    padding: '10px', 
+    position: 'fixed', // Cambia a 'fixed' para que siempre esté en la parte inferior
+    bottom: '0', // Lo coloca en la parte inferior
+    left: '0', // Lo alinea a la izquierda
+    right: '0', // Lo alinea a la derecha
+    zIndex: 2, 
+    borderRadius: '20px', // Bordes redondeados solo en la parte superior
+    margin: '10px', 
+    maxHeight: '80vh', 
+    overflowY: 'auto',
+    boxShadow: '0px -5px 10px rgba(0, 0, 0, 0.1)' // Opcional: añade una sombra para mejor visibilidad
+  }}
+>
+  {/* El resto de tu código permanece igual */}
+  <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
+    <MenuIcon onClick={toggleMenu} />
+    <div style={{ maxWidth: '30vw', overflow: 'auto' }}>
+      <ImageAndText content={content} onItemClick={handleItemClick} />
+    </div>
+    <div className="input-container backgroundColor2" onClick={(e) => e.stopPropagation()}>
+      <SeacrhTagInDb tags={tags} setTags={setTags} setContent={setContent} setMusicContent={setMusicContent} />
+    </div>
+    <ToggleIconOpenAndClose size={30} isOpen={isContentVisible} onToggle={toggleContentVisibility} />
+    <div className={isVideoFullScreen ? 'video-fullscreen' : 'video-normal'} style={{ position: isVideoFullScreen ? 'fixed' : 'relative', top: isVideoFullScreen ? '0' : 'auto', left: isVideoFullScreen ? '0' : 'auto', zIndex: isVideoFullScreen ? 9999 : 'auto' }}>
+      <Video 
+        tags={tags} 
+        setTags={setTags} 
+        setCurrentIndex={setCurrentIndex} 
+        currentIndex={currentIndex} 
+        currentTimeMedia={currentTimeMedia} 
+        setCurrentTimeMedia={setCurrentTimeMedia} 
+        componentInUse={componentInUse} 
+        setComponentInUse={setComponentInUse} 
+        id={content[0].videoPrincipal.id} 
+        src={content[0].videoPrincipal.src} 
+        style={{ width: '100%', height: '100%' }} 
+        className={[]} 
+        onClick={() => console.log('Video clicked')} 
+        setIsLoading={setIsLoading} 
+        isVideoFullScreen={isVideoFullScreen} 
+        allMusicProyects={musicContent} 
+        setContent={setContent} 
+        isEndendVideo={isEndedVideo} 
+        setIsEndedVideo={setIsEndedVideo} 
+        setMusicContent={setMusicContent} 
+      />
+      <div onClick={toggleVideoFullScreen} style={{ position: 'absolute', top: '0px', right: '0', zIndex: 10000, cursor: 'pointer', backgroundColor: 'none', padding: '0px', display: 'flex', alignItems: 'center', justifyContent: 'center', width: 'auto' }}>
+        {isVideoFullScreen ? (
+          <ShrinkIcon onClick={toggleVideoFullScreen} size={50} />
+        ) : (
+          <ExpandIcon onClick={toggleVideoFullScreen} size={50} />
+        )}
+      </div>
+    </div>
+  </div>
+  <div style={{ opacity: isContentVisible ? 1 : 0, maxHeight: isContentVisible ? '70vh' : '0', overflow: 'hidden', transition: 'opacity 2s ease, max-height 2s ease', display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}>
+    <MidiAndPdf content={content} onItemClick={handleItemClick} />
+  </div>
+  <Audio 
+    setCurrentIndex={setCurrentIndex} 
+    currentIndex={currentIndex} 
+    id={content[0].audioPrincipal.id} 
+    src={content[0].audioPrincipal.src} 
+    autoPlay={content[0].audioPrincipal.autoPlay} 
+    loop={content[0].audioPrincipal.loop} 
+    controlsList={content[0].audioPrincipal.controlsList} 
+    backgroundColor={content[0].audioPrincipal.backgroundColor} 
+    buttonColor={content[0].audioPrincipal.buttonColor} 
+    sliderEmptyColor={content[0].audioPrincipal.sliderEmptyColor} 
+    sliderFilledColor={content[0].audioPrincipal.sliderFilledColor} 
+    showPlayButton={content[0].audioPrincipal.showPlayButton} 
+    showVolumeButton={content[0].audioPrincipal.showVolumeButton} 
+    playIcon={content[0].audioPrincipal.playIcon} 
+    pauseIcon={content[0].audioPrincipal.pauseIcon} 
+    volumeIcon={content[0].audioPrincipal.volumeIcon} 
+    width={content[0].audioPrincipal.width} 
+    allMusicProyects={musicContent} 
+    setContent={setContent} 
+    setCurrentTimeMedia={setCurrentTimeMedia} 
+    currentTimeMedia={currentTimeMedia} 
+    setComponentInUse={setComponentInUse} 
+    componentInUse={componentInUse} 
+    setIsLoading={setIsLoading} 
+    isEndedVideo={isEndedVideo} 
+    setIsEndedVideo={setIsEndedVideo} 
+  />
+</div>
               {/* Menú lateral */}
               <Menu isOpen={isMenuOpen} onClose={toggleMenu} className='backgroundColor2' />
               <div style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, backdropFilter: 'blur(10px)', backgroundColor: 'rgba(0, 0, 0, 0.5)', display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: 9999, opacity: isLoading ? 1 : 0, visibility: isLoading ? 'visible' : 'hidden', transition: 'opacity 0.5s ease, visibility 0.5s ease', }}>
